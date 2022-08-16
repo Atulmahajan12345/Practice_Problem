f2bc6542af66c431b84dcb65e25951059ea9be3e
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-16 16:45:12 +0530

    Added:Sum Of Natural Numbers

---
+++ src/com/bridgelabz/sumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class sumOfNaturalNumbers {
+	public static void main(String[] args) {
+		int i, num = 100, sum = 0;
+		for (i = 1; i <= num; ++i) {
+		sum = sum + i;
+		}
+		System.out.println("Sum of First 100 Natural Numbers is = " + sum);
+	}
+}
